beb6029c869a35d8e04be2a8e2d0c6f0d578e4d8
Teby00 <tokkenpage@gmail.com>
2024-10-16 05:23:58 -0400

    🏘️ agrego action deleteComentario

--- app/post/[id]/actions.tsx
+++ app/post/[id]/actions.tsx
@@ -5,12 +5,11 @@ import { OnlyContenidoSchema } from '@/lib/schemas';
 import { cookies } from 'next/headers';
 import { revalidateTag } from 'next/cache';
 
-const token = cookies().get('session')?.value;
-
 export async function createComentario(
   data: InferInput<typeof OnlyContenidoSchema>,
   id: string
 ): Promise<string | void> {
+  const token = cookies().get('session')?.value;
   const response = await fetch(
     process.env.BACKEND_URL! + 'comentarios/' + id + '/',
     {
@@ -28,6 +27,31 @@ export async function createComentario(
     }
     if (response.status === 404) {
       return 'No existe el usuario.';
+    }
+    return 'Error inesperado.';
+  }
+
+  revalidateTag(`post-${id}`);
+}
+
+export async function deleteComentario(id: number): Promise<string | void> {
+  const token = cookies().get('session')?.value;
+  const response = await fetch(
+    process.env.BACKEND_URL! + 'comentarios/eliminar/' + id + '/',
+    {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  if (!response.ok) {
+    if (response.status === 401) {
+      return 'No autorizado.';
+    }
+    if (response.status === 404) {
+      return 'No existe el comentario.';
     }
     return 'Error inesperado.';
   }
