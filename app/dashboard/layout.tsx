c9762098ddbdcbdb8930d8e2b578a7ffe488fa52
Teby00 <tokkenpage@gmail.com>
2024-10-16 06:29:17 -0400

    🎢 agrego seguridad a la ruta de usuarios

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,15 @@
 import SideBar from '@/components/SideBar';
+import { getSession } from '@/lib/getSession';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { isAdmin } = await getSession();
   return (
     <div className="relative z-10">
-      <SideBar />
+      <SideBar isAdmin={isAdmin} />
       <main className="ml-[300px] py-8 px-10">{children}</main>
     </div>
   );
